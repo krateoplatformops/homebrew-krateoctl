10641fe0d81fb50d7c954f957cd771cd022ccd07
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2026-04-02 08:43:16 +0200

    feat: update krateoctl version to 1.1.2 and corresponding URLs

--- Formula/krateoctl.rb
+++ Formula/krateoctl.rb
@@ -1,12 +1,12 @@
 class Krateoctl < Formula
   desc "Krateo PlatformOps CLI"
   homepage "https://github.com/krateoplatformops/krateoctl/"
-  version "1.1.0"
+  version "1.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/krateoplatformops/krateoctl/releases/download/v1.1.0/krateoctl_1.1.0_darwin_arm64.tar.gz"
-      sha256 "9c83e41fd8365f1d217894b758f2977b1c12964bf8184c3c8e87c024b0db77d5"
+      url "https://github.com/krateoplatformops/krateoctl/releases/download/v1.1.2/krateoctl_1.1.2_darwin_arm64.tar.gz"
+      sha256 "92be26d7719810b761e86303c6d11d553cf19b2b9bc0b0ce04e8d04689cdf4a6"
 
       def install
         bin.install "krateoctl"
@@ -14,8 +14,8 @@ class Krateoctl < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/krateoplatformops/krateoctl/releases/download/v1.1.0/krateoctl_1.1.0_darwin_amd64.tar.gz"
-      sha256 "a65f39f530f34323369d51384c2fa3e94c586f0af66061979a6b33dbf0eb4a96"
+      url "https://github.com/krateoplatformops/krateoctl/releases/download/v1.1.2/krateoctl_1.1.2_darwin_amd64.tar.gz"
+      sha256 "f39754cc4b99f7066612cce0d9c5f9ad576cde441787e2f66dbe2da44669c23a"
 
       def install
         bin.install "krateoctl"
